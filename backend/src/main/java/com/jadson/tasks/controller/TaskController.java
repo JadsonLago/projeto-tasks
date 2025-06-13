f8ed42eadc13060af32d52bfc39f35c5b718cf3e
JadsonLago <jadsonlago.consultoria@gmail.com>
2025-06-12 23:02:44 -0300

    feat: implementação do controlador Task com endpoints para Tasks

---
+++ backend/src/main/java/com/jadson/tasks/controller/TaskController.java
@@ -0,0 +1,57 @@
+package com.jadson.tasks.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jadson.tasks.model.Task;
+import com.jadson.tasks.repository.TaskRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000") // Allow all origins for simplicity; adjust as needed
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    // Define your API endpoints here
+    @GetMapping
+    public List<Task> getAllTasks() {
+
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskRepository.findById(id).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
